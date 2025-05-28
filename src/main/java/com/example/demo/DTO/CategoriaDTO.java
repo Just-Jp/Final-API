24c5c202c97a27a93cf887a3d50a6b2617cbd5d0
Adriana Franco <adri.franco2@yahoo.com.br>
2025-05-27 21:22:36 -0300

    criando controller categoria

---
+++ src/main/java/com/example/demo/DTO/CategoriaDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CategoriaDTO {
+
+	
+    private Long id;
+    
+    @NotBlank(message = "O nome da categoria é obrigatório")
+    private String nome;
+    
+    public CategoriaDTO () {};
+    
+	public CategoriaDTO(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+    
+}
