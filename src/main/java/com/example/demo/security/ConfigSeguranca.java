7fa496c0c867ae6df26b76c560e5bb770250edf4
DragonN3xus <bernardorbennes@gmail.com>
2025-05-30 20:55:28 -0300

    Implementando Jwt

--- src/main/java/com/example/demo/security/ConfigSeguranca.java
+++ src/main/java/com/example/demo/security/ConfigSeguranca.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +23,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class ConfigSeguranca {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -33,6 +41,12 @@ public class ConfigSeguranca {
                     requests.anyRequest().authenticated();
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilter(new JwtAutenticationFilter(
+                authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), jwtUtil));
+        http.addFilter(new JwtAuthorizationFilter(
+                authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)),
+                jwtUtil, userDetailsService));
+
         return http.build();
     }
 
